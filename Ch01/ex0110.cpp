2f44b9ad7759c881dd385187e370d74ca987d853
deng25 <globlexmd@gmail.com>
2016-11-07 18:10:35 +0800

    Create ex0110.cpp

---
+++ Ch01/ex0110.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0, val = 10;
+    while (val >= 0) {
+        std::cout << val << std::endl;
+        val--;
+    }
+    std::cout << "Done" << std::endl;
+
+    return 0;
+}
