ce4d3ba8d69334bc6a6d8663dc77ad25c8254a48
deng25 <globlexmd@gmail.com>
2016-11-07 18:35:41 +0800

    Create 0113-3.cpp

---
+++ Ch01/0113-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int v1 = 0, v2 = 0;
+    std::cout << "请输入两个整数：" << std::endl;
+    std::cout << "整数1：";
+    std::cin >> v1;
+    std::cout << "整数2：";
+    std::cin >> v2;
+    std::cout << v1 << "和" << v2 << "之间的所有整数有：" << std::endl;
+    for (; v1 <= v2; v1++) {
+        std::cout << v1 << " ";        
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
