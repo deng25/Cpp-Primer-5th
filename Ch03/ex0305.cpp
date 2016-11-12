924d6137ed2c4266a21dc397d140aee4164924eb
deng25 <globlexmd@gmail.com>
2016-11-13 01:16:52 +0800

    Create ex0305.cpp

---
+++ Ch03/ex0305.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using std::string;
+using std::getline;
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+    string str, sum, sum2;
+        
+    cout << "请输入任意字符串（按Ctrl + Z结束输入）：";
+    if (cin >> str) {
+        sum2 = sum = str;
+        while (cin >> str) {
+            sum += str;
+            sum2 = sum2 + " " + str;
+        }
+    }
+    
+    cout << sum << endl;
+    cout << sum2 << endl;
+    return 0;
+}
