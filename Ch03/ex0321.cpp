28ea1d073e0f54b9de668d6aa61a4a509505a15b
deng25 <globlexmd@gmail.com>
2016-11-21 01:33:28 +0800

    Create ex0321.cpp

---
+++ Ch03/ex0321.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main()
+{
+    vector<int> iv;
+    int n;
+    cout << "请输入一组整数（按Ctrl + Z结束输入）：" << endl;
+
+    while (cin >> n) {
+        iv.push_back(n);
+    }
+
+    cout << "容器的容量为" << iv.size() << "，其中包含的元素有：" << endl;
+    for (auto iter = iv.cbegin(); iter != iv.cend(); ++iter) {
+        cout << *iter << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
