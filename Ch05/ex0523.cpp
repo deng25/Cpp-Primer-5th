567c7ba86e74813d8aa513df4105e50f311abcee
deng25 <globlexmd@gmail.com>
2016-11-30 19:09:43 +0800

    Create ex0523.cpp

---
+++ Ch05/ex0523.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int n1, n2;
+    cout << "请输入两个整数：" << endl;
+    cin >> n1 >> n2;
+    cout << n1 << '/' << n2 << '=' << n1 / n2 << endl;
+
+    return 0;
+}
