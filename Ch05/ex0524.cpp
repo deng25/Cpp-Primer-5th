9a3a91380c3224c6f3d4704d73a49c3b13cbc802
deng25 <globlexmd@gmail.com>
2016-11-30 19:25:52 +0800

    Update ex0524.cpp

--- Ch05/ex0524.cpp
+++ Ch05/ex0524.cpp
@@ -8,18 +8,11 @@ using std::exception;
 int main()
 {
     int n1, n2;
-    try {
-        cout << "请输入两个整数：" << endl;
-        cin >> n1 >> n2;
-
-        if (!n2) {
-            throw exception("被除数不能是0！");
-        }
+    cout << "请输入两个整数：" << endl;
+    cin >> n1 >> n2;
+    if (!n2) {
+        throw exception("被除数不能是0！");
     }
-    catch (exception err) {
-        cout << err.what() << endl;
-    }
-
     cout << n1 << '/' << n2 << '=' << n1 / n2 << endl;
 
     return 0;
